56d9790a4580e94039e25e67a17f9aa1820d684c
O6lvl4 <hiromi.motodera@aid-on.org>
2026-02-21 22:45:02 +0900

    Add doc generation step and GitHub Pages deployment

--- build.zig
+++ build.zig
@@ -30,4 +30,13 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_option_tests.step);
+
+    // Docs
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = lib.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+    const docs_step = b.step("docs", "Generate API documentation");
+    docs_step.dependOn(&install_docs.step);
 }
